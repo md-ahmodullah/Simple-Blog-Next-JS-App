c51610ed4cd7adf15c5e507c192675fafd4ea8f3
md-ahmodullah <contactme.ahmod@gmail.com>
2025-01-24 17:16:32 +0600

    Protected Profile Routes

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,4 +1,28 @@
+"use client";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Profile() {
+  const { isAuthenticated, isLoading } = useKindeAuth();
+  const router = useRouter();
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      router.push("/login");
+    }
+  }, [isAuthenticated, isLoading, router]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <span className="loading loading-dots loading-lg"></span>
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return null;
+  }
   return (
     <>
       <div className="w-10/12 mx-auto h-[500px]">
